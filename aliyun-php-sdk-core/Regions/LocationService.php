605185e96ba7f475b5ce3e8966440f677fe0975b
Simone Cabrino <cabrinosimone@gmail.com>
2018-04-24 13:35:11 +0300

    Fix PHP Notice in LocationService.php
    Quick fix, to resolve the PHP Notice:  Undefined index: cn-hongkong#Vpc in aliyun-php-sdk-core/Regions/LocationService.php on line 82

--- aliyun-php-sdk-core/Regions/LocationService.php
+++ aliyun-php-sdk-core/Regions/LocationService.php
@@ -79,7 +79,7 @@ class LocationService
 
     private function checkCacheIsExpire($key)
     {
-        $lastClearTime = self::$lastClearTimePerProduct[$key];
+        $lastClearTime = isset(self::$lastClearTimePerProduct[$key]) ? self::$lastClearTimePerProduct[$key] : null;
         if ($lastClearTime == null) {
             $lastClearTime = time();
             self::$lastClearTimePerProduct[$key] = $lastClearTime;
